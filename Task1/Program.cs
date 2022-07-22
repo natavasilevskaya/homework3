4fc01a2a444e9430e16b3d2a6ecc3e723f674112
natavasilevskaya <107160563+natavasilevskaya@users.noreply.github.com>
2022-07-22 03:12:22 +0300

    Task1 file added

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает на вход пятизначное число и проверяет, является ли оно палиндромом (работает только с 5-значными натуральными 
+//положительными числами)
+
+void PalindromCheck(string num)
+{
+if (num[0]==num[4] || num[1]==num[3])
+{
+    Console.WriteLine("Number " + num + " is palindrom!");
+}
+else 
+{
+    Console.WriteLine("Number " + num + " is not palindrom!");
+}
+}
+
+Console.WriteLine("Input five digit number:");
+string num = Console.ReadLine();
+
+PalindromCheck(num);
+
