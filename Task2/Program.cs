f641758686765ffae33f6138aaa97fe4d780735f
natavasilevskaya <107160563+natavasilevskaya@users.noreply.github.com>
2022-07-22 03:12:51 +0300

    Task2 file added

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт кубы чисел от 1 до N.
+
+void СubeNumber (int num)
+{
+    for (int i = 1 ;i<=num;i++)
+    {
+        Console.WriteLine(i * i * i + " ");
+    }
+}
+
+Console.WriteLine("Input number: ");
+int сube = Convert.ToInt32(Console.ReadLine());
+
+СubeNumber (сube);
